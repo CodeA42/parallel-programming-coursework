c7c74130bfe0b32e3f42716e50f64c31afb75888
Alexandar Velichkov <stu-1901321002@uni-plovdiv.bg>
2023-05-28 16:53:38 +0300

    Replace task-lean code with openmp contents

--- coursework/openmp/task-lean.cpp
+++ coursework/openmp/task-lean.cpp
@@ -18,13 +18,35 @@ struct Vec
     y = y_;
     z = z_;
   }
-  Vec operator+(const Vec &b) const { return Vec(x + b.x, y + b.y, z + b.z); }
-  Vec operator-(const Vec &b) const { return Vec(x - b.x, y - b.y, z - b.z); }
-  Vec operator*(double b) const { return Vec(x * b, y * b, z * b); }
-  Vec mult(const Vec &b) const { return Vec(x * b.x, y * b.y, z * b.z); }
-  Vec &norm() { return *this = *this * (1 / sqrt(x * x + y * y + z * z)); }
-  double dot(const Vec &b) const { return x * b.x + y * b.y + z * b.z; } // cross:
-  Vec operator%(Vec &b) { return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
+  Vec operator+(const Vec &b) const
+  {
+    return Vec(x + b.x, y + b.y, z + b.z);
+  }
+  Vec operator-(const Vec &b) const
+  {
+    return Vec(x - b.x, y - b.y, z - b.z);
+  }
+  Vec operator*(double b) const
+  {
+    return Vec(x * b, y * b, z * b);
+  }
+  Vec mult(const Vec &b) const
+  {
+    return Vec(x * b.x, y * b.y, z * b.z);
+  }
+  Vec &norm()
+  {
+    return *this = *this * (1 / sqrt(x * x + y * y + z * z));
+  }
+  // cross:
+  double dot(const Vec &b) const
+  {
+    return x * b.x + y * b.y + z * b.z;
+  }
+  Vec operator%(Vec &b)
+  {
+    return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
+  }
 };
 
 /**
@@ -171,7 +193,7 @@ inline Vec calculateIdealDiffuseReflection(unsigned short *Xi, Vec nl, const Sph
  * @param Xi
  * @return Vec
  */
-Vec radiance(const Ray &ray, int depth, unsigned short *Xi)
+Vec radiance(const Ray &ray, int depth, unsigned short *seed)
 {
   double distToIntersection;
   int idOfIntersectedObj;
@@ -183,7 +205,7 @@ Vec radiance(const Ray &ray, int depth, unsigned short *Xi)
                                                       : f.z; // max refl
   if (++depth > 5)
   {
-    if (erand48(Xi) < p)
+    if (erand48(seed) < p)
     {
       f = f * (1 / p);
     }
@@ -194,49 +216,54 @@ Vec radiance(const Ray &ray, int depth, unsigned short *Xi)
   }                               // R.R.
   if (obj.reflectionType == DIFF) // Ideal DIFFUSE reflection
   {
-    return calculateIdealDiffuseReflection(Xi, nl, obj, f, x, depth);
+    return calculateIdealDiffuseReflection(seed, nl, obj, f, x, depth);
   }
   else if (obj.reflectionType == SPEC) // Ideal SPECULAR reflection
   {
-    return obj.emission + f.mult(radiance(Ray(x, ray.direction - n * 2 * n.dot(ray.direction)), depth, Xi));
+    return obj.emission + f.mult(radiance(Ray(x, ray.direction - n * 2 * n.dot(ray.direction)), depth, seed));
   }
   Ray reflRay(x, ray.direction - n * 2 * n.dot(ray.direction)); // Ideal dielectric REFRACTION
   bool into = n.dot(nl) > 0;                                    // Ray from outside going in?
   double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = ray.direction.dot(nl), cos2t;
   if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0) // Total internal reflection
   {
-    return obj.emission + f.mult(radiance(reflRay, depth, Xi));
+    return obj.emission + f.mult(radiance(reflRay, depth, seed));
   }
   Vec tdir = (ray.direction * nnt - n * ((into ? 1 : -1) * (ddn * nnt + sqrt(cos2t)))).norm();
   double a = nt - nc, b = nt + nc, R0 = a * a / (b * b), c = 1 - (into ? -ddn : tdir.dot(n));
   double Re = R0 + (1 - R0) * c * c * c * c * c, Tr = 1 - Re, P = .25 + .5 * Re, RP = Re / P, TP = Tr / (1 - P);
-  return obj.emission + f.mult(depth > 2 ? (erand48(Xi) < P ? // Russian roulette
-                                                radiance(reflRay, depth, Xi) * RP
-                                                            : radiance(Ray(x, tdir), depth, Xi) * TP)
-                                         : radiance(reflRay, depth, Xi) * Re + radiance(Ray(x, tdir), depth, Xi) * Tr);
+  return obj.emission + f.mult(depth > 2 ? (erand48(seed) < P ? // Russian roulette
+                                                radiance(reflRay, depth, seed) * RP
+                                                              : radiance(Ray(x, tdir), depth, seed) * TP)
+                                         : radiance(reflRay, depth, seed) * Re + radiance(Ray(x, tdir), depth, seed) * Tr);
 }
 
 int main(int argc, char *argv[])
 {
-  int w = 1024, h = 768, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
-  Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm());        // cam pos, dir
-  Vec cx = Vec(w * .5135 / h), cy = (cx % cam.direction).norm() * .5135, r, *c = new Vec[w * h];
-  for (int y = 0; y < h; y++) // Loop over image rows
+  int width = 1024;
+  int height = 768;
+  int samples = argc == 2 ? atoi(argv[1]) / 4 : 1;
+  Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm()); // cam pos, dir
+  Vec cx = Vec(width * .5135 / height);
+  Vec cy = (cx % cam.direction).norm() * .5135;
+  Vec r;
+  Vec *c = new Vec[width * height];
+  for (int y = 0; y < height; y++) // Loop over image rows
   {
-    fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1));
-    for (unsigned short x = 0, Xi[3] = {0, 0, y * y * y}; x < w; x++) // Loop cols
+    fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samples * 4, 100. * y / (height - 1));
+    for (unsigned short x = 0, Xi[3] = {0, 0, y * y * y}; x < width; x++) // Loop cols
     {
-      for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++) // 2x2 subpixel rows
+      for (int sy = 0, i = (height - y - 1) * width + x; sy < 2; sy++) // 2x2 subpixel rows
       {
         for (int sx = 0; sx < 2; sx++, r = Vec()) // 2x2 subpixel cols
         {
-          for (int s = 0; s < samps; s++)
+          for (int s = 0; s < samples; s++)
           {
             double r1 = 2 * erand48(Xi), dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
             double r2 = 2 * erand48(Xi), dy = r2 < 1 ? sqrt(r2) - 1 : 1 - sqrt(2 - r2);
-            Vec d = cx * (((sx + .5 + dx) / 2 + x) / w - .5) +
-                    cy * (((sy + .5 + dy) / 2 + y) / h - .5) + cam.direction;
-            r = r + radiance(Ray(cam.origin + d * 140, d.norm()), 0, Xi) * (1. / samps);
+            Vec d = cx * (((sx + .5 + dx) / 2 + x) / width - .5) +
+                    cy * (((sy + .5 + dy) / 2 + y) / height - .5) + cam.direction;
+            r = r + radiance(Ray(cam.origin + d * 140, d.norm()), 0, Xi) * (1. / samples);
           } // Camera rays are pushed ^^^^^ forward to start in interior
           c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
         }
@@ -244,8 +271,8 @@ int main(int argc, char *argv[])
     }
   }
   FILE *f = fopen("image.ppm", "w"); // Write image to PPM file.
-  fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
-  for (int i = 0; i < w * h; i++)
+  fprintf(f, "P3\n%d %d\n%d\n", width, height, 255);
+  for (int i = 0; i < width * height; i++)
   {
     fprintf(f, "%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
   }
